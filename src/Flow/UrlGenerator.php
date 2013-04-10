60b2e2eee90073e2fdc00c8a53e6d08807276a1d
Jonathan Goodger <jonno.is@gmail.com>
2013-04-10 21:37:00 +0100

    Update UrlGenerator to handle Doctrine proxies

--- src/Flow/UrlGenerator.php
+++ src/Flow/UrlGenerator.php
@@ -6,20 +6,18 @@ class UrlGenerator
 {
     public function generate($entity)
     {
-        switch(get_class($entity)) {
-            case 'Flow\Entities\Series':
-                return '/' . $entity->getSlug();
-            case 'Flow\Entities\Chapter':
-                return '/' . $entity->getSeries()->getSlug()
-                     . '/' . $entity->getChapterNo();
-            case 'Flow\Entities\Page':
-                return '/' . $entity->getChapter()->getSeries()->getSlug()
-                     . '/' . $entity->getChapter()->getChapterNo()
-                     . '/' . $entity->getPageNo();
-            default:
-                throw new \InvalidArgumentException(
-                    'Unrecognised object passed to URL Generator: ' . get_class($entity)
-                );
-        }
+        if ($entity instanceof Entities\Series)
+            return '/' . $entity->getSlug();
+        if ($entity instanceof Entities\Chapter)
+            return '/' . $entity->getSeries()->getSlug()
+                 . '/' . $entity->getChapterNo();
+        if ($entity instanceof Entities\Page)
+            return '/' . $entity->getChapter()->getSeries()->getSlug()
+                 . '/' . $entity->getChapter()->getChapterNo()
+                 . '/' . $entity->getPageNo();
+
+        throw new \InvalidArgumentException(
+            'Unrecognised object passed to URL Generator: ' . get_class($entity)
+        );
     }
 }
